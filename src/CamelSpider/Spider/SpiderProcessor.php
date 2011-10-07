e27d21dc594ada33395887d98e110ddf82252865
Gilmar Pupo <gilmar@gilmar.eti.br>
2011-10-07 01:03:18 -0300

    validando url

--- src/CamelSpider/Spider/SpiderProcessor.php
+++ src/CamelSpider/Spider/SpiderProcessor.php
@@ -6,32 +6,81 @@ class SpiderProcessor
 {
 	protected $goutte; 
     
-	protected $curl;
+	protected $logger;
 	/**
 	* Recebe instância de https://github.com/fabpot/Goutte
+	* e do Monolog
 	**/
-    public function __construct($goutte)
+    public function __construct($goutte, $logger)
     {
         $this->goutte = $goutte;
-		$this->curl = $this->goutte->getNamedClient('curl');
+	    $this->logger = $logger;
 		return $this;
     }
 
+	protected function logger($string, $type = 'info')
+	{
+		return $this->logger->$type('#CamelSpiderProcessor ' . $string);
+	}
 
 	public function getCrawler($URI, $mode = 'GET')
 	{
-		return $this->curl->request($mode, $URI);
+		
+		// reiniciar instancia
+		//$this->goutte->insulate();
+		
+	   	return  $this->goutte->request($mode, $URI);
+		
 	}
-	
-	public function checkUpdates($subscription)
+
+	protected function getLinks($crawler)
 	{
+		$aCollection = $crawler->filter('a');
 		
-		$crawler = $this->getCrawler('http://www.terra.com.br/portal/');
-		$response = $crawler->getResponse();
-	    $content = $crawler->getContent();
+		$this->logger( 'links:' . $aCollection->count());
 		
+		$aCollection->reduce(function ($node, $i)
+		{
+            $href = $node->getAttribute('href');
+			if (empty($href) || 
+			in_array($href,array('#', ""))) {
+        		return false;
+        	}
+	    });
 		
-		return $content;
+		$links = $aCollection->each(function ($node, $i){
+		    return $node->getAttribute('href');
+			});
+		
+		return $links;
+		
+	}
+    protected $recursive = 2;
+	public function checkUpdates($URI, $recursive = 0)
+	{
+
+		$staticRecursive = $recursive + 1;
+		$crawler = $this->getCrawler($URI);
+		$data = '';
+
+        //Instrospecção
+        if($staticRecursive <= $this->recursive){
+
+		    foreach($this->getLinks($crawler) as $link)
+		    {
+				echo $link . "\n";
+		    	 //$this->goutte->insulate();	
+		    	 //$data .= $this->checkUpdates($link, $staticRecursive);
+		    }
+        }    
+		//var_dump($data);
+		//$this->goutte->getResponse()->getContent();
+		#$response = $crawler->getResponse();
+		//$client->getContainer();
+	    #$content = $crawler->getContent();
+		//var_dump($crawler->getResponse());
+		
+		return $data;
 	
 	}
 	
